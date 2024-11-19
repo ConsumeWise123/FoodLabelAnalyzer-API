9731874a9d8ebf9a0cd955dd9a966cecb9b02e15
shivanis1406 <61353420+shivanis1406@users.noreply.github.com>
2024-11-20 00:36:06 +0530

    Update claims_analysis.py

--- api/claims_analysis.py
+++ api/claims_analysis.py
@@ -1,6 +1,6 @@
 import logging
 import traceback
-import sys, pickle
+import sys
 from functools import wraps
 import os
 import json
@@ -27,7 +27,7 @@ def create_assistant(client):
     vector_store3 = client.beta.vector_stores.create(name="Misleading Claims Vec")
     
     # Ready the files for upload to OpenAI
-    file_paths = ["MisLeading_Claims.docx"]
+    file_paths = ["docs/MisLeading_Claims.docx"]
     file_streams = [open(path, "rb") for path in file_paths]
     
     # Use the upload and poll SDK helper to upload the files, add them to the vector store,
@@ -43,44 +43,6 @@ def create_assistant(client):
     )
 
     return assistant3
-
-def get_files_with_ingredient_info(ingredient, embeddings_titles_list, N=1):
-
-    embeddings_titles_1 = embeddings_titles_list[0]
-    with open('docs/titles.txt', 'r') as file:
-        lines = file.readlines()
-    
-    titles = [line.strip() for line in lines]
-    folder_name_1 = "docs/articles"
-    #Apply cosine similarity between embedding of ingredient name and title of all files
-    file_paths_abs_1, file_titles_1, refs_1 = find_relevant_file_paths(ingredient, embeddings_titles_1, titles, folder_name_1, journal_str = ".ncbi.", N=N)
-
-    embeddings_titles_2 = embeddings_titles_list[1]
-    with open('docs/titles_harvard.txt', 'r') as file:
-        lines = file.readlines()
-    
-    titles = [line.strip() for line in lines]
-    folder_name_2 = "docs/articles_harvard"
-    #Apply cosine similarity between embedding of ingredient name and title of all files
-    file_paths_abs_2, file_titles_2, refs_2 = find_relevant_file_paths(ingredient, embeddings_titles_2, titles, folder_name_1, N=N)
-
-    #Fine top N titles that are the most similar to the ingredient's name
-    #Find file names for those titles
-    file_paths = []
-    refs = []
-    if len(file_paths_abs_1) == 0 and len(file_paths_abs_2) == 0:
-        file_paths.append("docs/Ingredients.docx")
-    else:
-        for file_path in file_paths_abs_1:
-            file_paths.append(file_path)
-        refs.extend(refs_1)
-        for file_path in file_paths_abs_2:
-            file_paths.append(file_path)
-        refs.extend(refs_2)
-
-        print(f"Titles are {file_titles_1} and {file_titles_2}")
-            
-    return file_paths, refs
   
 def analyze_harmful_ingredients(ingredient, assistant_id, client):
     
@@ -181,109 +143,6 @@ def analyze_harmful_ingredients(ingredient, assistant_id, client):
     for key, value in harmful_ingredient_analysis.items():
       harmful_ingredient_analysis_str += f"{key}: {value}\n"
     return harmful_ingredient_analysis_str, is_ingredient_not_found_in_doc
-
-
-def get_assistant_for_ingredient(ingredient, client, embeddings_titles_list, N=2):
-  
-    #Harmful Ingredients
-    assistant2 = client.beta.assistants.create(
-      name="Harmful Ingredients",
-      instructions=f"You are an expert dietician. Use your knowledge base to answer questions about the ingredient {ingredient} in a food product.",
-      model="gpt-4o",
-      tools=[{"type": "file_search"}],
-      temperature=0,
-      top_p = 0.85
-      )
-
-    # Create a vector store
-    vector_store2 = client.beta.vector_stores.create(
-     name="Harmful Ingredients Vec",
-     chunking_strategy={
-        "type": "static",
-        "static": {
-            "max_chunk_size_tokens": 400,  # Set your desired max chunk size
-            "chunk_overlap_tokens": 200    # Set your desired overlap size
-        }
-    }
-    )
-
-    # Ready the files for upload to OpenAI.     
-    file_paths, refs = get_files_with_ingredient_info(ingredient, embeddings_titles_list, N)
-    #if file_paths[0] == "Ingredients.docx" and assistant_default_doc:
-        #print(f"Using Ingredients.docx for analyzing ingredient {ingredient}")
-    #    return assistant_default_doc, refs
-        
-    print(f"DEBUG : Creating vector store for files {file_paths} to analyze ingredient {ingredient}")
-    
-    file_streams = [open(path, "rb") for path in file_paths]
-    
-    # Use the upload and poll SDK helper to upload the files, add them to the vector store,
-    # and poll the status of the file batch for completion.
-    file_batch2 = client.beta.vector_stores.file_batches.upload_and_poll(
-      vector_store_id=vector_store2.id, files=file_streams
-    )
-    
-    # You can print the status and the file counts of the batch to see the result of this operation.
-    print(file_batch2.status)
-    print(file_batch2.file_counts)
-
-    #harmful Ingredients
-    assistant2 = client.beta.assistants.update(
-      assistant_id=assistant2.id,
-      tool_resources={"file_search": {"vector_store_ids": [vector_store2.id]}},
-    )
-
-    #if file_paths[0] == "docs/Ingredients.docx" and assistant_default_doc is None:
-    #    assistant_default_doc = assistant2
-        
-    return assistant2, refs
-
-def analyze_processing_level(ingredients, assistant_id, client):
-    
-    thread = client.beta.threads.create(
-        messages=[
-            {
-                "role": "user",
-                "content": "Categorize food product that has following ingredients: " + ', '.join(ingredients) + " into Group A, Group B, or Group C based on the document. The output must only be the group category name (Group A, Group B, or Group C) alongwith the reason behind assigning that respective category to the product. If the group category cannot be determined, output 'NOT FOUND'.",
-            }
-        ]
-    )
-    
-    run = client.beta.threads.runs.create_and_poll(
-        thread_id=thread.id,
-        assistant_id=assistant_id,
-        include=["step_details.tool_calls[*].file_search.results[*].content"]
-    )
-
-    # Polling loop to wait for a response in the thread
-    messages = []
-    max_retries = 10  # You can set a maximum retry limit
-    retries = 0
-    wait_time = 2  # Seconds to wait between retries
-
-    while retries < max_retries:
-        messages = list(client.beta.threads.messages.list(thread_id=thread.id, run_id=run.id))
-        if messages:  # If we receive any messages, break the loop
-            break
-        retries += 1
-        time.sleep(wait_time)
-
-    # Check if we got the message content
-    if not messages:
-        raise TimeoutError("Processing Level : No messages were returned after polling.")
-        
-    message_content = messages[0].content[0].text
-    annotations = message_content.annotations
-    #citations = []
-    for index, annotation in enumerate(annotations):
-        message_content.value = message_content.value.replace(annotation.text, "")
-        #if file_citation := getattr(annotation, "file_citation", None):
-        #    cited_file = client.files.retrieve(file_citation.file_id)
-        #    citations.append(f"[{index}] {cited_file.filename}")
-
-    print(message_content.value)
-    processing_level_str = message_content.value
-    return processing_level_str
 
 # Define the request body using a simple BaseModel (without complex pydantic models if not needed)
 class IngredientAnalysisRequest(BaseModel):
