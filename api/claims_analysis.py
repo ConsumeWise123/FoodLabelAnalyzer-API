c12444978d423864f69d3cb0991e72b6f240e8c0
shivanis1406 <61353420+shivanis1406@users.noreply.github.com>
2024-11-20 00:41:57 +0530

    Update claims_analysis.py

--- api/claims_analysis.py
+++ api/claims_analysis.py
@@ -44,60 +44,32 @@ def create_assistant(client):
 
     return assistant3
   
-def analyze_harmful_ingredients(ingredient, assistant_id, client):
-    
-    is_ingredient_not_found_in_doc = False
+def analyze_claims(claims, ingredients, assistant_id, client):
     
     thread = client.beta.threads.create(
         messages=[
             {
                 "role": "user",
-                "content": "A food product has the ingredient: " + ingredient + ". Is this ingredient safe to eat? The output must be in JSON format: {<ingredient_name>: <information from the document about why ingredient is harmful>}. If information about an ingredient is not found in the documents, the value for that ingredient must start with the prefix '(NOT FOUND IN DOCUMENT)' followed by the LLM's response based on its own knowledge.",
+                "content": "A food product named has the following claims: " + ', '.join(claims) + " and ingredients: " + ', '.join(ingredients) + """. Please evaluate the validity of each claim as well as assess if the product name is misleading.
+The output must be in JSON format as follows: 
+
+{
+  <claim_name>: {
+    'Verdict': <A judgment on the claim's accuracy, ranging from 'Accurate' to varying degrees of 'Misleading'>,
+    'Why?': <A concise, bulleted summary explaining the specific ingredients or aspects contributing to the discrepancy>,
+    'Detailed Analysis': <An in-depth explanation of the claim, incorporating relevant regulatory guidelines and health perspectives to support the verdict>
+  }
+}
+"""
             }
-        ]
+                ]
     )
     
     run = client.beta.threads.runs.create_and_poll(
         thread_id=thread.id,
         assistant_id=assistant_id,
-        include=["step_details.tool_calls[*].file_search.results[*].content"],
-        tools=[{
-        "type": "file_search",
-        "file_search": {
-            "max_num_results": 5
-        }
-        }]
+        include=["step_details.tool_calls[*].file_search.results[*].content"]
     )
-    
-    
-    ## List run steps to get step IDs
-    #run_steps = client.beta.threads.runs.steps.list(
-    #    thread_id=thread.id,
-    #    run_id=run.id
-    #)
-    
-    ## Initialize a list to store step IDs and their corresponding run steps
-    #all_steps_info = []
-    
-    ## Iterate over each step in run_steps.data
-    #for step in run_steps.data:  # Access each RunStep object
-    #    step_id = step.id  # Get the step ID (use 'step_id' instead of 'id')
-    
-        ## Retrieve detailed information for each step using its ID
-        #run_step_detail = client.beta.threads.runs.steps.retrieve(
-        #    thread_id=thread.id,
-        #    run_id=run.id,
-        #    step_id=step_id,
-        #    include=["step_details.tool_calls[*].file_search.results[*].content"]
-        #)
-    
-        ## Append a tuple of (step_id, run_step_detail) to the list
-        #all_steps_info.append((step_id, run_step_detail))
-    
-    ## Print all step IDs and their corresponding run steps
-    #for step_id, run_step_detail in all_steps_info:
-    #    print(f"Step ID: {step_id}")
-    #    print(f"Run Step Detail: {run_step_detail}\n")
     
     # Polling loop to wait for a response in the thread
     messages = []
@@ -114,68 +86,64 @@ def analyze_harmful_ingredients(ingredient, assistant_id, client):
 
     # Check if we got the message content
     if not messages:
-        raise TimeoutError("Processing Ingredients : No messages were returned after polling.")
+        raise TimeoutError("Processing Claims : No messages were returned after polling.")
         
     message_content = messages[0].content[0].text
-    annotations = message_content.annotations
-
-    #citations = []
-
-    #print(f"Length of annotations is {len(annotations)}")
-
-    for index, annotation in enumerate(annotations):
-      if file_citation := getattr(annotation, "file_citation", None):
-          #cited_file = client.files.retrieve(file_citation.file_id)
-          #citations.append(f"[{index}] {cited_file.filename}")
-          message_content.value = message_content.value.replace(annotation.text, "")
-  
-    ingredients_not_found_in_doc = []        
-    print(message_content.value)
-    for key, value in json.loads(message_content.value.replace("```", "").replace("json", "")).items():
-        if value.startswith("(NOT FOUND IN DOCUMENT)"):
-            ingredients_not_found_in_doc.append(key)
-            is_ingredient_not_found_in_doc = True
-        print(f"Ingredients not found in database {','.join(ingredients_not_found_in_doc)}")
     
-    harmful_ingredient_analysis = json.loads(message_content.value.replace("```", "").replace("json", "").replace("(NOT FOUND IN DOCUMENT) ", ""))
-        
-    harmful_ingredient_analysis_str = ""
-    for key, value in harmful_ingredient_analysis.items():
-      harmful_ingredient_analysis_str += f"{key}: {value}\n"
-    return harmful_ingredient_analysis_str, is_ingredient_not_found_in_doc
+      
+    annotations = message_content.annotations
+    
+    #citations = []
+    
+    #print(f"Length of annotations is {len(annotations)}")
+    
+    for index, annotation in enumerate(annotations):
+          if file_citation := getattr(annotation, "file_citation", None):
+              #cited_file = client.files.retrieve(file_citation.file_id)
+              #citations.append(f"[{index}] {cited_file.filename}")
+              message_content.value = message_content.value.replace(annotation.text, "")
+      
+    #if debug_mode:
+    #    claims_not_found_in_doc = []
+    #    print(message_content.value)
+    #    for key, value in json.loads(message_content.value.replace("```", "").replace("json", "")).items():
+    #          if value.startswith("(NOT FOUND IN DOCUMENT)"):
+    #              claims_not_found_in_doc.append(key)
+    #    print(f"Claims not found in the doc are {','.join(claims_not_found_in_doc)}")
+    #claims_analysis = json.loads(message_content.value.replace("```", "").replace("json", "").replace("(NOT FOUND IN DOCUMENT) ", ""))
+    claims_analysis = {}
+    if message_content.value != "":
+        claims_analysis = json.loads(message_content.value.replace("```", "").replace("json", ""))
+
+    claims_analysis_str = ""
+    for key, value in claims_analysis.items():
+      claims_analysis_str += f"{key}: {value}\n"
+    
+    return claims_analysis_str
 
 # Define the request body using a simple BaseModel (without complex pydantic models if not needed)
-class IngredientAnalysisRequest(BaseModel):
+class ClaimsAnalysisRequest(BaseModel):
     product_info_from_db: dict
     
-@app.post("/api/processing_level-ingredient-analysis")
-def get_ingredient_analysis(request: IngredientAnalysisRequest):
+@app.post("/api/claims-analysis")
+def get_claims_analysis(request: ClaimsAnalysisRequest):
     product_info_from_db = request.product_info_from_db
         
     if product_info_from_db:
         brand_name = product_info_from_db.get("brandName", "")
         product_name = product_info_from_db.get("productName", "")
+        claims_list = product_info_from_db.get("claims", [])
         ingredients_list = [ingredient["name"] for ingredient in product_info_from_db.get("ingredients", [])]
 
-        processing_level = ""
-        all_ingredient_analysis = ""
         claims_analysis = ""
-        refs = []
         
-        if len(ingredients_list) > 0:
+        if len(claims_list) > 0:
             #Create client
             client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
             #Create assistant for processing level
-            assistant_p, embeddings_titles_list = create_assistant_and_embeddings(client, ['docs/embeddings.pkl', 'docs/embeddings_harvard.pkl'])
+            assistant_c = create_assistant(client)
             #Create embeddings
-  
-            processing_level = analyze_processing_level(ingredients_list, assistant_p.id, client) if ingredients_list else ""
-            for ingredient in ingredients_list:
-                assistant_id_ingredient, refs_ingredient = get_assistant_for_ingredient(ingredient, client, embeddings_titles_list, 2)
-                ingredient_analysis, is_ingredient_in_doc = analyze_harmful_ingredients(ingredient, assistant_id_ingredient.id, client)
-                all_ingredient_analysis += ingredient_analysis + "\n"
-                if is_ingredient_in_doc:
-                    refs.extend(refs_ingredient)
+            claims_analysis = analyze_claims(claims_list, ingredients_list, assistant_c.id, client) if claims_list else ""
 
-        return {'refs' : refs, 'all_ingredient_analysis' : all_ingredient_analysis, 'processing_level' : processing_level}
+        return {'claims' : claims_analysis}
