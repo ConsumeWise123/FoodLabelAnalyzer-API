ff9ce2c3687702c5f6e63c798a39e28cbfb5d9dd
shivanis1406 <61353420+shivanis1406@users.noreply.github.com>
2024-11-19 17:32:09 +0530

    Update nutrient_analyzer.py

--- api/nutrient_analyzer.py
+++ api/nutrient_analyzer.py
@@ -108,6 +108,8 @@ async def get_nutrient_analysis(product_info: Dict[str, Any]):
     try:
         if ("nutritionalInformation" not in product_info or "servingSize" not in product_info or "quantity" not in product_info["servingSize"]):
             return ""
+        if (len(product_info["nutritionalInformation"]) == 0 or product_info["servingSize"]["quantity"] == 0):
+            return ""
             
         nutritional_information = product_info["nutritionalInformation"]
         logger.debug(f"Processing nutritional information: {nutritional_information}")
