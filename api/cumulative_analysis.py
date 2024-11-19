bbfff3d564391fe3bc1cd133e478505f45e2ddc8
shivanis1406 <61353420+shivanis1406@users.noreply.github.com>
2024-11-20 01:53:52 +0530

    Update cumulative_analysis.py

--- api/cumulative_analysis.py
+++ api/cumulative_analysis.py
@@ -90,6 +90,7 @@ Claims Analysis for the product is as follows ->
 
     if len(refs) > 0:
         L = min(2, len(refs))
-        return f"Brand: {brand_name}\n\nProduct: {product_name}\n\nAnalysis:\n\n{completion.choices[0].message.content}\n\nTop Citations:\n\n{'\n'.join(refs[0:L])}"
+        refs_str = '\n'.join(refs[0:L])
+        return f"Brand: {brand_name}\n\nProduct: {product_name}\n\nAnalysis:\n\n{completion.choices[0].message.content}\n\nTop Citations:\n\n{refs_str}"
     else:
         return f"Brand: {brand_name}\n\nProduct: {product_name}\n\nAnalysis:\n\n{completion.choices[0].message.content}"
